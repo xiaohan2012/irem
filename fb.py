8823e22500dc0efa88b3142eca55a55914fef520
xiaohan <xiaohan2012@gmail.com>
2013-07-16 12:51:32 +0800

    forward algorithm written but test not passed

---
+++ fb.py
@@ -0,0 +1,33 @@
+import numpy as np
+
+from lmatrix import LMatrix
+
+START = "START"
+END = "END"
+
+def forward_prob_table(obs, A, B):
+    """
+    (observations: list of obj,
+    State transition matrix: LMatrix,
+    state emission matrix: LMatrix) =>
+    forward prob table: LMatrix
+    """
+    states = A.rlabels
+    #forward prob table
+    ft = LMatrix(rlabels = states, #states as row
+                 clabels = range(len(obs))) #observations as columns
+
+    
+    print states, B.clabels, ft
+    
+    for s in states:
+        ft[s, 0] = A[START,s] * B[s, obs[0]]
+
+    for prev_i, i in zip(range(len(obs) -1 ), range(1, len(obs))):
+        ob = obs[i]
+        for s in states:
+            print s, ft[:,prev_i], A[:, s], ob, B[s, ob]
+            ft[s,i] = sum(ft[:,prev_i] * A[:, s] * B[s, ob])
+    
+    ft[END, -1] = sum(ft[:, -1] * A[:, END])
+    return ft
---
+++ test/t_fb.py
@@ -0,0 +1,44 @@
+from fb import *
+
+from lmatrix import LMatrix
+
+from common import *
+
+class ForwardAlgorithmTest(unittest.TestCase):
+    def setUp(self):
+        self.A = LMatrix(rlabels = [START, "H", "C", END],
+                         data = np.array([
+                             [0, 0.8, 0.2, 0],
+                             [0, 0.7, 0.3, 0.5],
+                             [0, 0.4, 0.6, 0.5],
+                             [0, 0.0, 0.0, 0]
+                         ]))
+        
+        self.B = LMatrix(rlabels = [START, "H", "C", END],
+                         clabels = ["1", "3"],
+                         data = np.array([
+                             [0, 0],
+                             [0.2, 0.4],
+                             [0.5, 0.1],
+                             [0, 0]
+                         ])
+        )
+
+    def test_result(self):
+        """test whether the forward prob table is calculated correctly"""
+        expected = LMatrix(rlabels = [START, "H", "C", END],
+                           clabels = range(3),
+                           data = np.array([
+                               [0, 0, 0],
+                               [0.32, 0.0464, 0.0049696],
+                               [0.02, 0.05400, 0.02316],
+                               [0, 0, 0.0140648]
+                           ])
+        )
+        
+        actual = forward_prob_table(obs = ["3", "1", "3"], A = self.A, B = self.B)
+        
+        self.assertEqual(expected, actual)
+
+if __name__ == '__main__':
+    unittest.main()
