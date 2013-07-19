77b45cadcc8a3ecd7acf896b88c2e69f51242880
xiaohan <xiaohan2012@gmail.com>
2013-07-19 18:53:18 +0800

    removed the annoying START and END from forward table + forward test data changed

--- fb.py
+++ fb.py
@@ -8,29 +8,33 @@ START = "START"
 END = "END"
 
 @memoized
-def forward_prob_table(obs, A, B):
+def forward_prob_table(obs, A, B, pi):
     """
-    (observations: list of obj,
-    State transition matrix: LMatrix,
-    state emission matrix: LMatrix) =>
+    (
+    obs, the observations: list of obj,
+    A, State transition matrix: LMatrix,
+    B, state emission matrix: LMatrix
+    pi, initial state vector: tuple of tuple)
+    =>
     forward prob table: LMatrix
     """
     states = A.rlabels
     
     #forward prob table
     ft = LMatrix(rlabels = states, #states as row
-                 clabels = range(len(obs))) #observations as columns
+                 clabels = range(len(obs)+1)) #observations as columns
 
     
     for s in states:
-        ft[s, 0] = A[START,s] * B[s, obs[0]]
+        #somewhat not easy to read, because pi must be hashable, so the original dict becomes a tuple
+        #my question: any hashable dict?
+        print s
+        ft[s, 0] = [i for i in pi if i[0] == s][0][1] 
 
-    for prev_i, i in zip(range(len(obs) -1 ), range(1, len(obs))):
-        ob = obs[i]
+    for i in xrange(1, len(obs)+1):
+        ob = obs[i-1]
         for s in states:
-            ft[s,i] = sum(ft[:,prev_i] * A[:, s] * B[s, ob])
-    
-    ft[END, -1] = sum(ft[:, -1] * A[:, END])
+            ft[s,i] = sum(ft[:,i-1] * A[:, s] * B[:, ob])
     
     return ft
 
@@ -54,7 +58,6 @@ def backward_prob_table(obs, A, B):
 
 def convergent(old_mat, new_mat):
     """whether two matrices are approximate enough"""
-    print (np.abs(old_mat - new_mat) / old_mat)
     return (np.abs(old_mat - new_mat) / old_mat < 1e-5).all()
     
 def forward_backward(lst_of_obs, V, Q, init_A, init_B):
@@ -81,6 +84,9 @@ def forward_backward(lst_of_obs, V, Q, init_A, init_B):
     B = init_B
     
     while True:
+        print "starting A "
+        print A
+        
         #expect
         @memoized
         def gamma(k, t, j):
@@ -104,19 +110,36 @@ def forward_backward(lst_of_obs, V, Q, init_A, init_B):
             return ft[j,t] * A[i,j] * B[j,obs[t+1]] * bt[j,t+1] / obs_prob
 
         #maximization
-        new_A = LMatrix(rlabels = Q)
-        new_B = LMatrix(rlabels = Q, clabels = V)
+        new_A = LMatrix(rlabels = A.rlabels)
+        new_B = LMatrix(rlabels = A.rlabels, clabels = V)
 
         for i in Q:
             for j in Q:
-                new_A[i,j] = sum((xi(k, t, i, j) for k in xrange(m) for t in xrange(len(lst_of_obs[k]) - 1))) / \
-                             sum((xi(k, t, i, j) for k in xrange(m) for t in xrange(len(lst_of_obs[k]) - 1) for j in Q ))
+                numer = sum((xi(k, t, i, j) for k in xrange(m) for t in xrange(len(lst_of_obs[k]) - 1)))
+                denom = sum((xi(k, t, i, j) for k in xrange(m) for t in xrange(len(lst_of_obs[k]) - 1) for j in Q ))
+                #print i, j, numer, denom
+                new_A[i,j] =  numer / denom
+                              
 
         for j in Q:
             for vk in V:
-                new_B[j,vk] = sum((gamma(k, t, j) for k in xrange(m) for t in xrange(len(lst_of_obs[k])) if lst_of_obs[k][t] == vk)) / \
-                              sum((gamma(k, t, j) for k in xrange(m) for t in xrange(len(lst_of_obs[k]))))
+                #print j, vk, numer, denom
+                numer = sum((gamma(k, t, j) for k in xrange(m) for t in xrange(len(lst_of_obs[k])) if lst_of_obs[k][t] == vk))
+                denom = sum((gamma(k, t, j) for k in xrange(m) for t in xrange(len(lst_of_obs[k]))))
+                new_B[j,vk] = numer / denom
+                
+        print "new A"
         print new_A
-        print new_B
-        if convergent(A, new_A) and convergent(B, new_B):
-            return new_A, new_B         
+        #print new_B
+
+        sliced_A = A[1:-1,1:-1]
+        sliced_new_A = new_A[1:-1,1:-1]
+        sliced_B = B[1:-1,1:-1]
+        sliced_new_B = new_B[1:-1,1:-1]
+        
+        if convergent(sliced_A, sliced_new_A) and convergent(B, new_B):
+            return new_A, new_B
+        break
+        A = new_A
+        B = new_B
+        print np.sum(sliced_A - sliced_new_A)
--- test/t_forward.py
+++ test/t_forward.py
@@ -6,39 +6,38 @@ from common import *
 
 class ForwardAlgorithmTest(unittest.TestCase):
     def setUp(self):
-        self.A = LMatrix(rlabels = [START, "H", "C", END],
+        self.pi = (("S1", 1), ("S2", 0.0))
+        
+        self.A = LMatrix(rlabels = ["S1", "S2"],
                          data = np.array([
-                             [0, 0.8, 0.2, 0],
-                             [0, 0.7, 0.3, 0.5],
-                             [0, 0.4, 0.6, 0.5],
-                             [0, 0.0, 0.0, 0]
+                             [0.7, 0.3],
+                             [0.5, 0.5]
                          ]))
         
-        self.B = LMatrix(rlabels = [START, "H", "C", END],
-                         clabels = ["1", "3"],
+        self.B = LMatrix(rlabels = ["S1", "S2"],
+                         clabels = ["K1", "K2", "K3"],
                          data = np.array([
-                             [0, 0],
-                             [0.2, 0.4],
-                             [0.5, 0.1],
-                             [0, 0]
+                             [0.6, 0.1, 0.3],
+                             [0.1, 0.7, 0.2],
                          ])
         )
 
     def test_result(self):
         """test whether the forward prob table is calculated correctly"""
-        expected = LMatrix(rlabels = [START, "H", "C", END],
-                           clabels = range(3),
+        expected = LMatrix(rlabels = ["S1", "S2"],
+                           clabels = range(4),
                            data = np.array([
-                               [0, 0, 0],
-                               [0.32, 0.0464, 0.021632],
-                               [0.02, 0.05400, 0.004632],
-                               [0, 0, 0.013132]
+                               [1, 0.21, 0.0462, 0.021294],
+                               [0.0, 0.09, 0.0378, 0.010206]
                            ])
         )
 
-        actual = forward_prob_table(("3", "1", "3"), self.A, self.B)
+        actual = forward_prob_table(("K3", "K2", "K1"), self.A, self.B, self.pi)
         
         self.assertEqual(expected, actual)
 
+class YetAnotherTest(unittest.TestCase):
+    pass
+    
 if __name__ == '__main__':
     unittest.main()
