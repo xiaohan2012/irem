03d952e12fcc74fd3989246e05fc2753a011b9cb
xiaohan <xiaohan2012@gmail.com>
2013-07-23 19:09:26 +0800

    new: backward vectorized

--- fb_vec.py
+++ fb_vec.py
@@ -16,5 +16,29 @@ def forward_vec(obs, A, B, pi):
         tbl[:,i] = np.matrix(tbl[:,i-1]) * A_mat * diag if i > 0 else pi * diag 
         tbl[:,i] = tbl[:,i] / sum(tbl[:,i])
         
-    obs_prob = 0
-    return tbl, obs_prob
+    return tbl
+
+
+def backward_vec(obs, A, B, pi):
+    Q = A.rlabels
+    V = B.clabels
+    T = len(obs)
+    
+    tbl = LMatrix(Q,xrange(T))
+
+    A_mat = np.matrix(A)
+
+    for i in range(T)[::-1]:
+        ob = obs[i]
+        O = np.diag(B[:,ob])
+
+        r = np.array(A_mat * O * np.matrix(tbl[:,i+1]).T) \
+                if i < T-1 else \
+                    np.array(A_mat * O * np.ones((len(Q),1)))
+
+        tbl[:,i] = r[:,0]
+        
+        tbl[:,i] = tbl[:,i] / sum(tbl[:,i])
+            
+    return tbl
+    
--- test/t_fb_vec.py
+++ test/t_fb_vec.py
@@ -3,9 +3,9 @@ test for the vectorized version of forward-backward prob calculation
 """
 
 from common import *
-from fb_vec import forward_vec
+from fb_vec import forward_vec, backward_vec
 
-class ForwardTest(unittest.TestCase):
+class ForwardBackwardTest(unittest.TestCase):
         
     def test_fb_table(self):
         obs, A, B, pi = weather_data()
@@ -18,10 +18,26 @@ class ForwardTest(unittest.TestCase):
                            ])
         )
 
-        actual, _ = forward_vec(obs, A, B, pi)
+        actual = forward_vec(obs, A, B, pi)
         for s in A.rlabels:
             for i in xrange(len(obs)):
                 self.assertAlmostEqual(expected[s,i], actual[s,i], 4)
+
+    def test_bb_table(self):
+        obs, A, B, pi = weather_data()
         
+        expected = LMatrix(rlabels = ["rain", "sunny"],
+                           clabels = range(len(obs)),
+                           data = np.array([
+                               [0.6469, 0.5923, 0.3763, 0.6533, 0.6273],
+                               [0.3531, 0.4077, 0.6237, 0.3467, 0.3727]
+                           ])
+        )
+
+        actual = backward_vec(obs, A, B, pi)
+        for s in A.rlabels:
+            for i in xrange(len(obs)):
+                self.assertAlmostEqual(expected[s,i], actual[s,i], 4)
+                
 if __name__ == '__main__':
     unittest.main()
