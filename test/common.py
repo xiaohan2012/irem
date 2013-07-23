4e63b6b42e7c5e48ae8bce28065ad69e617a471b
xiaohan <xiaohan2012@gmail.com>
2013-07-23 18:01:38 +0800

    created weather data set

--- test/common.py
+++ test/common.py
@@ -7,3 +7,23 @@ sys.path.append("..")
 
 from lmatrix import LMatrix
 from util import *
+
+def weather_data():
+    pi = np.matrix([0.5, 0.5])
+        
+    A = LMatrix(rlabels = ["rain", "sunny"],
+                data = np.array([
+                    [0.7, 0.3],
+                    [0.3, 0.7]
+                ]))
+        
+    B = LMatrix(rlabels = ["rain", "sunny"],
+                clabels = ["umbrella", "no-umbrella"],
+                data = np.array([
+                    [0.9, 0.1],
+                    [0.2, 0.8],
+                ]))
+
+    obs = ("umbrella", "umbrella", "no-umbrella", "umbrella", "umbrella")
+
+    return obs, A, B, pi
